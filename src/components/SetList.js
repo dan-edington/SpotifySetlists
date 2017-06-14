aab08a8042c6cc666d1bf06cacc3476f964f5824
Dan Edington <dan2505@hotmail.com>
2017-06-14 14:07:50 +0100

    Added songs and save playlist button

--- src/components/SetList.js
+++ src/components/SetList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const SetList = (props) => {
-console.log(props);
+
+  let songList = props.setData.songLists.main.map((song, i) => {
+    return(
+      <p key={i}>{song}</p>
+    )
+  })
+
   return(
     <div>
       <h4>{ props.artistName } @ { props.setData.venue.name }, { props.setData.venue.city }</h4>
-      <p>{ props.setData.date }</p>
+      <h5>{ props.setData.date }</h5>
+      {songList}
+      <button>Save as playlist</button>
       <hr />
     </div>
   );
