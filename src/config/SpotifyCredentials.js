363852a903caab7f5216d856e475079fe4da519e
Dan Edington <dan2505@hotmail.com>
2017-09-11 19:22:00 +0100

    updated oauth key

--- src/config/SpotifyCredentials.js
+++ src/config/SpotifyCredentials.js
@@ -1,12 +1,12 @@
 const SpotifyCredentials = {
   production: {
     clientID: 'cee894f275734341a6dd69a408adbc70',
-    clientSecret: '9abd04578d1341c6bc6c3544a7d17bd0',
+    clientSecret: '51b9f52e3eb34a74bc674fcbd1dfe70b',
     callbackURL: 'http://www.danedington.com/spotifySetlists/',
   },
   development: {
     clientID: 'cee894f275734341a6dd69a408adbc70',
-    clientSecret: '9abd04578d1341c6bc6c3544a7d17bd0',
+    clientSecret: '51b9f52e3eb34a74bc674fcbd1dfe70b',
     callbackURL: 'http://localhost:3000/auth/spotify/callback',
   },
 };
