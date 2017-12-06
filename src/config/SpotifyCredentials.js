e1d5c8734d5570108d424871ee1b6a719f41dc31
Dan Edington <dan2505@hotmail.com>
2017-12-06 11:55:37 +0000

    updated .gitignore

--- src/config/SpotifyCredentials.js
+++ src/config/SpotifyCredentials.js
@@ -2,7 +2,7 @@ const SpotifyCredentials = {
   production: {
     clientID: 'cee894f275734341a6dd69a408adbc70',
     clientSecret: '51b9f52e3eb34a74bc674fcbd1dfe70b',
-    callbackURL: 'http://www.danedington.com/spotifySetlists/',
+    callbackURL: 'http://spotify-setlists.danedington.com/',
   },
   development: {
     clientID: 'cee894f275734341a6dd69a408adbc70',
